eccda89b0d8945d70ec7bf7318761c362fdb1f9d
iuwyr <168219681+iuwyr@users.noreply.github.com>
2025-05-16 22:43:26 +0300

    Update config.cmake

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -2,6 +2,6 @@ hunter_cacheable(true)
 
 hunter_download(
     PACKAGE_NAME primality
-    URL "https://github.com/YOUR_USERNAME/lab07hw/archive/v1.0.0.tar.gz"
-    SHA1 ""
+    URL "https://github.com/iuwyr/lab07hw/archive/refs/tags/v1.0.1.tar.gz"
+    SHA1 "d0a3a20a32ff731047a93dced9f091640cbc2736"
 )
